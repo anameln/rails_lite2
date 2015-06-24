5f7be3b1dda1c344180c9387d1ac42ad3d88a968
= <=>
2015-06-24 18:02:12 -0400

    Phase 6

--- lib/phase6/router.rb
+++ lib/phase6/router.rb
@@ -3,15 +3,24 @@ module Phase6
     attr_reader :pattern, :http_method, :controller_class, :action_name
 
     def initialize(pattern, http_method, controller_class, action_name)
+      @pattern, @http_method, @controller_class, @action_name = pattern, http_method, controller_class, action_name
     end
 
     # checks if pattern matches path and method matches request method
     def matches?(req)
+      method = req.request_method.to_sym
+      method = http_method
+      patterns = ["/users", "/users/new", "/users/(\d+)", "/users/(\d+)/edit"]
+      patterns.any? { |pattern| Regexp.new(@pattern).match(pattern)}
     end
 
     # use pattern to pull out route params (save for later?)
     # instantiate controller and call controller action
     def run(req, res)
+      @pattern.def method_name
+
+      end
+      ControllerBase.new(req, res)
     end
   end
 
